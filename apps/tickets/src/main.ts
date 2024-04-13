a11621ed8ff8f8b36a26503dd669edc8271cf3a7
daadaamed <daadaamed@gmail.com>
2024-04-13 23:07:55 +0200

    fix changes

--- apps/tickets/src/main.ts
+++ apps/tickets/src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3003);
+  await app.listen(3004);
   console.log(`Ticket management app is running on port 3003`);
 }
 bootstrap();
